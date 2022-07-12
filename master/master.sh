e10af80dcaabc388d05cdfd5e68a8a82487a7264
LoveesYe <109154433+LoveesYe@users.noreply.github.com>
2022-07-12 18:21:15 +0200

    Update master.sh

--- master/master.sh
+++ master/master.sh
@@ -219,7 +219,7 @@ else
 fi
 
 cd /opt/
-download "https://github.com/LoveesYe/cdnflydadao/master/$tar_gz_name" "https://github.com/LoveesYe/cdnflydadao/master/$tar_gz_name" "$tar_gz_name"
+download "https://github.com/LoveesYe/cdnflydadao/raw/main/master/$tar_gz_name" "https://github.com/LoveesYe/cdnflydadao/raw/main/master/$tar_gz_name" "$tar_gz_name"
 
 tar xf $tar_gz_name
 rm -rf cdnfly
